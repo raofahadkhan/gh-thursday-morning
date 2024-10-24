729166e9e1b3f6b9ec8f3a11eaa68b5b16aa81e8
Fahad Khan <raofahad046@gmail.com>
2024-10-24 15:24:39 +0500

    class-7 complete code

---
+++ class-7/app/page.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="bg-fahad
+     h-[400px] bg-no-repeat	w-full bg-cover">
+      {/* png jpg jpeg svg webp */}
+      <h2
+        className="my-[10%] text-primary text-[36px] 
+      p-1"
+      >
+        Hello World
+      </h2>
+    </div>
+    // px (pixel per inch)
+    // % percentage (percentage of the parent)
+  );
+}
+// Border is consisted of 3 properties
+// 1. Thickness.
+// 2. Style eg: solid, lined, dashed.
+// 3. Color eg: black, blue, yellow.
