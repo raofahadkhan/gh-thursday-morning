81d81b7e79ea5de61cc6858c1c1647d9bbe637cc
Fahad Khan <raofahad046@gmail.com>
2024-10-31 12:18:15 +0500

    class-8 complete code

---
+++ class-8/app/page.tsx
@@ -0,0 +1,50 @@
+// import React from "react";
+// import Image from "next/image";
+
+// function Home() {
+//   return (
+//     <div>
+//       {/* <img src="/images/demo.png" alt="image" /> */}
+//       <Image
+//         src={"/images/demo.png"}
+//         alt="next_image"
+//         width={600}
+//         height={600}
+//       />
+//     </div>
+//   );
+// }
+
+// export default Home;
+
+import React from "react";
+import Header from "@/components/Header";
+import Image from "next/image";
+
+function Home() {
+  return (
+    <div className="h-screen">
+      <Header />
+      <div className="flex h-[80%]">
+        <div className="w-1/2 flex flex-col justify-center items-start m-12">
+          <h1 className="text-[44px] font-bold">Welcome To Our Website</h1>
+          <p className="text-[32px] w-[571px]">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard
+          </p>
+          <button>Contact US</button>
+        </div>
+        <div className="w-1/2 flex justify-center items-center">
+          <Image
+            src={"/images/figma_image.svg"}
+            alt="hero image"
+            width={502}
+            height={465}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ class-8/components/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Header() {
+  return <div className="h-[88px] bg-slate-500"></div>;
+}
+
+export default Header;
