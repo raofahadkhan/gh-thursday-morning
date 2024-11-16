29f2296f244d52e22999c00aa8b41091537197e8
Fahad Khan <raofahad046@gmail.com>
2024-11-16 18:26:22 +0500

    updates

--- class-8/app/page.tsx
+++ class-8/app/page.tsx
@@ -26,13 +26,13 @@ function Home() {
     <div className="h-screen">
       <Header />
       <div className="flex h-[80%]">
-        <div className="w-1/2 flex flex-col justify-center items-start m-12">
+        <div className="w-1/2 flex flex-col justify-center items-start m-12 gap-7">
           <h1 className="text-[44px] font-bold">Welcome To Our Website</h1>
           <p className="text-[32px] w-[571px]">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard
           </p>
-          <button>Contact US</button>
+          <button className="bg-[#121212] w-[237px] h-[66px] text-white">Contact US</button>
         </div>
         <div className="w-1/2 flex justify-center items-center">
           <Image
